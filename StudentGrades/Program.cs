42b977c7459d7a7e4eb8eec7b672d6a8d245d3f0
tamma-byte <93988570+tamma-byte@users.noreply.github.com>
2022-05-26 14:51:18 +1000

    Initial commit

---
+++ StudentGrades/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Ashley Barty", "Business", 3.6);
+            Student student2 = new Student("Venus Williams", "PE", 2.7);
+
+            //ONLY SHOW Students that have Honours
+            if(student1.HasHonours() == true)
+            {
+                Console.WriteLine(student1.name + " has honours.");
+            }
+
+            if (student2.HasHonours() == true)
+            {
+                Console.WriteLine(student2.name + " has honours.");
+            }
+
+            //Console.WriteLine(student1.name + " has honours: " +student1.HasHonours());
+            //Console.WriteLine(student2.name + " has honours: " +student2.HasHonours());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StudentGrades/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentGrades
+{
+    class Student
+    {
+        public string name;
+        public string subject;
+        public double grade;
+
+        public Student(string cName, string cSubject, double cGrade)
+        {
+            name = cName;
+            subject = cSubject;
+            grade = cGrade;
+        }
+
+        //RETURN WHETHER A STUDENT HAS HONOURS OR NOT
+        public bool HasHonours()
+        {
+            if(grade >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
